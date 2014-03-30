bcad22e7ccce6c1c095693f8d950766040388705
sam-ginsburg <sbg11@duke.edu>
2014-03-30 01:14:36 -0400

    first button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 window.addEventListener('initdone', start);
 window.addEventListener('goalreached', this.goalReached);
 
+$('.one').click(saveButton);
+
 FileSystem.init();
 
 function start(){
 
 }
 
-function goalReached(event){
+function goalReached(event) {
 
 	$(".modal-body").html("Congrats! You have reached your " + event.detail.name + " goal! You have earned " + event.detail.pts + " points.");
 	$('#myModal').modal('toggle');
 
+}
+
+function saveButton(event) {
+	// event.preventDefault();
+	var amt = $('#amountSave').val();
+	var goalname = $('#goalC').val();
+	FileSystem.saveMoney(goalname, parseInt(amt,10));
+	return false;
 }
