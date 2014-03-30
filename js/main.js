037817014e6023833d4173f136752ed794902c3a
Chris-Dee <cmd45@duke.edu>
2014-03-30 04:40:04 -0400

    added stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('initdone', start);
 window.addEventListener('goalreached', this.goalReached);
-
+window.wtf = "wtf";
 FileSystem.init();
 
 function start(){
@@ -13,3 +13,17 @@ function goalReached(event){
 	$('#myModal').modal('toggle');
 
 }
+
+
+
+window.populateGoals = function(allGoals){
+	var goals = $('#goals');
+	for(var i=0;i<allGoals.length;i++){
+		var goal = allGoals[i];
+		html1='<span>'+goal.name+'</span>'
+		goals.append($(html1));
+		html = '<div class="progress progress-striped active"><div class="progress-bar" role="progressbar" aria-valuenow="'+goal.current+'" aria-valuemin="0" aria-valuemax="100" style="width:'+(goal.current/goal.cost)*100+'%"></div>'
+		goals.append($(html));
+	}
+}
+
